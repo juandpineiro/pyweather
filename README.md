fbc17d48ace1454207e77acce2926ab1a2635687
Juan Piñeiro <juandpineiro@gmail.com>
2020-01-01 22:38:49 -0400

    Modify image and icons locations

---
+++ README.md
@@ -0,0 +1,3 @@
+# PyWeatherTk
+
+A Python 3 GUI application (made with Tkinter) that uses the [OpenWeatherMap](https://openweathermap.org/) API to present current weather conditions in the entered city
--- weather.py
+++ weather.py
@@ -24,7 +24,7 @@ def format_response(weather):
 
 def open_image(icon):
     size = int(lower_frame.winfo_height()*0.25)
-    img = ImageTk.PhotoImage(Image.open('./img/'+icon+'.png').resize((size, size)))
+    img = ImageTk.PhotoImage(Image.open('./img/wi/'+icon+'.png').resize((size, size)))
     weather_icon.delete("all")
     weather_icon.create_image(0,0, anchor='nw', image=img)
     weather_icon.image = img
@@ -42,9 +42,10 @@ def get_weather(city):
 # Tkinter interface
 
 root = tk.Tk()
+root.title("PyWeather")
 
 canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
-background_image = ImageTk.PhotoImage(Image.open("landscape.jpg"))
+background_image = ImageTk.PhotoImage(Image.open("./img/landscape.png"))
 background_label = tk.Label(root, image=background_image)
 background_label.place(relwidth=1, relheight=1)
 canvas.pack()
@@ -52,19 +53,19 @@ canvas.pack()
 frame = tk.Frame(root, bg="#80c1ff", bd=5)
 frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor="n")
 
-entry = tk.Entry(frame, font=40)
+entry = tk.Entry(frame)
 entry.place(relwidth=0.65, relheight=1)
 
-button = tk.Button(frame, text="Get Weather", font=40, command=lambda: get_weather(entry.get()))
+button = tk.Button(frame, text="Get Weather", command=lambda: get_weather(entry.get()))
 button.place(relx=0.7, relwidth=0.3, relheight=1)
 
 lower_frame = tk.Frame(root, bg="#80c1ff", bd=10)
 lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor="n")
 
-results = tk.Label(lower_frame, font=("Courier", 18), anchor="nw", justify="left", bd=5)
+results = tk.Label(lower_frame, anchor="nw", justify="left", bd=5, bg="white")
 results.place(relwidth=1, relheight=1)
 
-weather_icon = tk.Canvas(results)
+weather_icon = tk.Canvas(results, bg="white")
 weather_icon.place(relx=.75, rely=0, relwidth=1, relheight=0.5)
 
 root.mainloop()
