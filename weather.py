764b26ff47f752d9b16cb29b5fd0cbeaee298ddd
Juan Piñeiro <juandpineiro@gmail.com>
2020-01-01 21:56:24 -0400

    Add weather icons

--- weather.py
+++ weather.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import tkinter as tk
 from tkinter import font
 from PIL import ImageTk, Image
@@ -6,8 +8,7 @@ import requests
 HEIGHT = 500
 WIDTH = 600
 
-def test_function(entry):
-    print("This is the entry:", entry)
+# API response handling and business logic
 
 def format_response(weather):
     try:
@@ -21,24 +22,32 @@ def format_response(weather):
 
     return final_str
 
+def open_image(icon):
+    size = int(lower_frame.winfo_height()*0.25)
+    img = ImageTk.PhotoImage(Image.open('./img/'+icon+'.png').resize((size, size)))
+    weather_icon.delete("all")
+    weather_icon.create_image(0,0, anchor='nw', image=img)
+    weather_icon.image = img
+
 def get_weather(city):
     weather_key = "edd0ba9793409519788a1956d25f4ed8"
     url = "https://api.openweathermap.org/data/2.5/weather"
     params = {"APPID": weather_key, "q": city, "units": "metric"}
     response = requests.get(url, params=params)
     weather = response.json()
-    label["text"] = format_response(weather)
+    results["text"] = format_response(weather)
+    icon_name = weather["weather"][0]["icon"]
+    open_image(icon_name)
 
-# api.openweathermap.org/data/2.5/forecast?q={city name},{country code} 
+# Tkinter interface
 
 root = tk.Tk()
 
 canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
-canvas.pack()
-
 background_image = ImageTk.PhotoImage(Image.open("landscape.jpg"))
 background_label = tk.Label(root, image=background_image)
 background_label.place(relwidth=1, relheight=1)
+canvas.pack()
 
 frame = tk.Frame(root, bg="#80c1ff", bd=5)
 frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor="n")
@@ -52,7 +61,10 @@ button.place(relx=0.7, relwidth=0.3, relheight=1)
 lower_frame = tk.Frame(root, bg="#80c1ff", bd=10)
 lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor="n")
 
-label = tk.Label(lower_frame, font=("Courier", 18), anchor="nw", justify="left", bd=5)
-label.place(relwidth=1, relheight=1)
+results = tk.Label(lower_frame, font=("Courier", 18), anchor="nw", justify="left", bd=5)
+results.place(relwidth=1, relheight=1)
+
+weather_icon = tk.Canvas(results)
+weather_icon.place(relx=.75, rely=0, relwidth=1, relheight=0.5)
 
 root.mainloop()
